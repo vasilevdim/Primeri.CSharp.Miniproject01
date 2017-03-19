f45e213904a45d8f04c1e38724b16418c8d262ea
vasilevdim <vasilevdim@gmail.com>
2017-03-19 22:55:46 +0300

    Modul06 Video02

---
+++ MP01/UserInput/UserInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UserInput
+{
+	public class UInput
+	{
+		public UInput()
+		{
+		}
+		public void justTesting()
+		{
+			Console.WriteLine ("Startirane na UserInput.UInput.justTesting()");
+		}
+
+	}
+}
