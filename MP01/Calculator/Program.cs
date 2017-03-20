3cc709d1a8bc4795ec3fb4b9f18039efbe7fc0ea
vasilevdim <vasilevdim@gmail.com>
2017-03-20 22:23:00 +0300

    Modul06 Video04

--- MP01/Calculator/Program.cs
+++ MP01/Calculator/Program.cs
@@ -4,9 +4,15 @@ namespace Calculator
 {
 	class MainClass
 	{
+		//Definirane na biblioteki
+		public UserInput.UInput uInput = new UserInput.UInput(); 
+
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			
+			MainClass _program = new MainClass();
+			_program.uInput.getUserCommands();
 		}
+
 	}
 }
--- MP01/UserInput/UserInput.cs
+++ MP01/UserInput/UserInput.cs
@@ -6,6 +6,28 @@ namespace UserInput
 		public UInput()
 		{
 		}
+
+		public void getUserCommands()
+		{
+			string _command = "";
+
+			do {
+				//Vzemane na komanda
+
+
+
+				Console.Write ("$ ");
+				_command = Console.ReadLine ();
+				//Proverka za nalichni komandi
+				if (_command.ToLower().Contains("command1")) Console.WriteLine("Startirana e komanda 1\n");
+				if (_command.ToLower().Contains("command2")) Console.WriteLine("Startirana e komanda 2\n");
+				if (_command.ToLower().Contains("command3")) Console.WriteLine("Startirana e komanda 3\n");
+
+
+			}while (_command.ToLower() != "exit");
+
+		}
+
 		public void justTesting()
 		{
 			Console.WriteLine ("Startirane na UserInput.UInput.justTesting()");
